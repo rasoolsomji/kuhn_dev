687bbda6a178542a64de20d804c6541c482a6253
mordaroso <mordaroso@gmail.com>
2021-07-25 15:34:02 +0000

    Deploying to gh-pages from @ mordaroso/kuhn_dev@70ef428f1dc8214bb171297347ce17bf1a0585d5 🚀

--- _next/static/6UFZRAfnqOCtP_uENM7wi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fa194ef5e94667e0b20d.css","static/chunks/pages/index-58be5627b222f37ff21a.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
