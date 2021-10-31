40de25c14e40ced36c173d99c45939012a7e0818
mordaroso <mordaroso@gmail.com>
2021-10-31 22:55:25 +0000

    Deploying to gh-pages from @ mordaroso/kuhn_dev@ff8636f446eacc48b3c43b9f318427bce791e8ea 🚀

---
+++ _next/static/gid9yrPrxblWDj2hzmfJ1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d193a36dc44361c3.css","static/chunks/pages/index-d084e16504a67417.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/yXOZSbAMCFKcP7wUGAGK2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d193a36dc44361c3.css","static/chunks/pages/index-b646bd1fc235d54c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
